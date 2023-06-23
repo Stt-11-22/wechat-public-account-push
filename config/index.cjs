36d0a7c456373977f569a8be44bd3c7140c7fe39
Stt-11-22 <106753145+Stt-11-22@users.noreply.github.com>
2023-06-23 20:14:20 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -51,7 +51,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '	7nbCKxLDSWWdlqiUXrrA5h_pqxM8TaXVAYMObiRViWk',
+  CALLBACK_TEMPLATE_ID: '7nbCKxLDSWWdlqiUXrrA5h_pqxM8TaXVAYMObiRViWk',
 
   CALLBACK_USERS: [
     {
