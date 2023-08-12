a706cff25b07ff9afb6e4f95dd58d2a5db04e141
Stt-11-22 <106753145+Stt-11-22@users.noreply.github.com>
2023-08-12 09:32:38 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'oy3Qc69VeZe1bHDjwERdgRNThFSY',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'kwH5vKgq96yW6TVEugpj9_4TK0QMInQDZUV3zA18sxI',
+      useTemplateId: 'sJU1mmoXRHvx8FDFwID5wy8Rr5nA7bcHQXIyY1tV3q0',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-27',
       festivals: [
@@ -43,17 +43,15 @@ const USER_CONFIG = {
       customizedDateList: [
         // 在一起的日子
         { keyword: 'love_day', date: '2023-05-31' },
-        // 距离下一次见面还有
-        { keyword: 'marry_day', date: '2023-08-26' },
-        // 距离老婆6月的大姨妈还有
-        { keyword: 'birth_day', date: '2023-07-29' }
+        // 距离大姨妈还有
+        { keyword: 'marry_day', date: '2023-08-31' },
       ],
     },
   ],
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: 'kwH5vKgq96yW6TVEugpj9_4TK0QMInQDZUV3zA18sxI',
+  CALLBACK_TEMPLATE_ID: 'sJU1mmoXRHvx8FDFwID5wy8Rr5nA7bcHQXIyY1tV3q0',
 
   CALLBACK_USERS: [
     {
